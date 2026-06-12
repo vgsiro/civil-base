5095b1ee51f95815591a9f57fa1c348f0881b6be
Vuong <tranvuong2832@gmail.com>
2026-06-12 15:23:16 +0800

    Fix social pages scroll: remove global overflow:hidden that broke document scroll

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
       lang="en"
       className={`${inter.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="h-full flex flex-col">
+      <body className="min-h-full flex flex-col">
         <LanguageProvider>{children}</LanguageProvider>
       </body>
     </html>
